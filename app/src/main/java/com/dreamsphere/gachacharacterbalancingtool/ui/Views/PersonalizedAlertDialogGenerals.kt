b7d7a71020c3bd291e79a4677e58ee49d3f45e57
Francesco <francesco.zerbinatti@gmail.com>
2023-07-14 10:41:37 +0200

    bugfix 14_07

--- app/src/main/java/com/dreamsphere/gachacharacterbalancingtool/ui/Views/PersonalizedAlertDialogGenerals.kt
+++ app/src/main/java/com/dreamsphere/gachacharacterbalancingtool/ui/Views/PersonalizedAlertDialogGenerals.kt
@@ -70,19 +70,19 @@ fun PersonalizedAlertDialogGenerals(
                             onClick = {
                                 if (type.equals(FACTIONS)){
                                     viewModel.faction.value= generalsListState.value.get(i)
-                                    if (viewModel.charactersListFlow.value.size>0){
+                                    if (viewModel.charactersListFlow.value.size>0&& index!!.toInt()>-1){
                                         viewModel.charactersListFlow.value[index!!.toInt()].character_faction=generalsListState.value.get(i)
                                     }
                                 }else
                                 if (type.equals(CLASSES)){
                                     viewModel.classs.value= generalsListState.value.get(i)
-                                    if (viewModel.charactersListFlow.value.size>0){
+                                    if (viewModel.charactersListFlow.value.size>0&& index!!.toInt()>-1){
                                         viewModel.charactersListFlow.value[index!!.toInt()].character_class=generalsListState.value.get(i)
                                     }
                                 }else
                                 if (type.equals(TIERS)){
                                     viewModel.tier.value= generalsListState.value.get(i)
-                                    if (viewModel.charactersListFlow.value.size>0){
+                                    if (viewModel.charactersListFlow.value.size>0&& index!!.toInt()>-1){
                                         viewModel.charactersListFlow.value[index!!.toInt()].character_tier=generalsListState.value.get(i)
                                     }
                                 }
